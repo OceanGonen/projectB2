6a1fa69f3d68167c7f1a2468e62d359d377ff4f7
OceanGonen <ocean.gonen@hotmail.com>
2024-01-23 15:34:06 +0100

    update icons

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+    //scroll to top button
     var backToTopButton = document.getElementById('top');
+    function toggleButtonVisibility() {
+        if (window.scrollY > 0) {
+            backToTopButton.classList.remove('hidden');
+        } else {
+            backToTopButton.classList.add('hidden');
+        }
+    }
+
   
     function scrollToTop() {
       window.scrollTo({
@@ -9,5 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   
     backToTopButton.addEventListener('click', scrollToTop);
+    document.addEventListener('scroll', toggleButtonVisibility);
+
+    //empty textbox
+    function clearText() {
+        document.getElementById('textBox').textContent = '';
+    }
   });
   
